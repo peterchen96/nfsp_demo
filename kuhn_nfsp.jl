8000f324464ce8cfa22b648cd44e2f36a69e12ad
peter <csherry1231@gmail.com>
2021-07-12 07:46:29 +0800

    simplify the experiment's code

--- kuhn_nfsp.jl
+++ kuhn_nfsp.jl
@@ -12,30 +12,34 @@ include("nfsp.jl")
 # Encode the KuhnPokerEnv's states for training.
 env = KuhnPokerEnv()
 states = [
-    [(), (:J,), (:J, :pass, :bet), (:Q,), (:Q, :pass, :bet), (:K,), (:K, :pass, :bet),
-    (:J, :bet, :bet), (:J, :bet, :pass), (:J, :pass, :bet), (:J, :pass, :pass), (:Q, :bet, :bet), 
-    (:Q, :bet, :pass), (:Q, :pass, :bet), (:Q, :pass, :pass), (:K, :bet, :bet), (:K, :bet, :pass), 
-    (:K, :pass, :bet), (:K, :pass, :pass)], # all states for player1
+    (), (:J,), (:Q,), (:K,),
+    (:J, :bet), (:J, :pass), (:Q, :bet), (:Q, :pass), (:K, :bet), (:K, :pass),
+    (:J, :pass, :bet), (:J, :bet, :bet), (:J, :bet, :pass), (:J, :pass, :pass),
+    (:Q, :pass, :bet), (:Q, :bet, :bet), (:Q, :bet, :pass), (:Q, :pass, :pass),
+    (:K, :pass, :bet), (:K, :bet, :bet), (:K, :bet, :pass), (:K, :pass, :pass),
+    (:J, :pass, :bet, :pass), (:J, :pass, :bet, :bet), (:Q, :pass, :bet, :pass),
+    (:Q, :pass, :bet, :bet), (:K, :pass, :bet, :pass), (:K, :pass, :bet, :bet),
+] # all states for players 1 & 2
 
-    [(), (:J, :bet), (:J, :pass), (:Q, :bet), (:Q, :pass), (:K, :bet), (:K, :pass),
-    (:J, :bet), (:J, :pass), (:J, :pass, :bet, :bet), (:J, :pass, :bet, :pass), (:Q, :bet), (:Q, :pass), 
-    (:Q, :pass, :bet, :bet), (:Q, :pass, :bet, :pass), (:K, :bet), (:K, :pass), (:K, :pass, :bet, :bet), 
-    (:K, :pass, :bet, :pass)] # all states for player2
-]
+states_indexes_Dict = Dict((i, j) for (j, i) in enumerate(states))
 
-states_indexes_Dict = [Dict([(i, j) for (j, i) in enumerate(s)]) for s in states]
+RLBase.state(env::StateTransformedEnv, args...; kwargs...) =
+    env.state_mapping(state(env.env, args...; kwargs...), args...)
+
+RLBase.state_space(env::StateTransformedEnv, args...; kwargs...) = 
+    env.state_space_mapping(state_space(env.env, args...; kwargs...), args...)
 
 wrapped_env = StateTransformedEnv(
         env;
-        state_mapping = s -> 
-        current_player(env) == chance_player(env) ? s : [states_indexes_Dict[current_player(env)][s]],
-        state_space_mapping = ss -> 
-        current_player(env) == chance_player(env) ? ss : 1:length(states[current_player(env)])
+        state_mapping = (s, player=current_player(env)) -> 
+            player == chance_player(env) ? s : [states_indexes_Dict[s]],
+        state_space_mapping = (ss, player=current_player(env)) -> 
+            player == chance_player(env) ? ss : [[i] for i in 1:length(states)]
     )
 
 # global parameters
 seed = 123
-anticipatory_param = 0.1
+anticipatory_param = 0.1f0
 used_device = Flux.cpu # Flux.gpu
 rng = StableRNG(seed)
 
@@ -48,6 +52,7 @@ nfsp = NFSPAgents(wrapped_env;
         _device = used_device, 
         ϵ_decay = ϵ_decay, 
         hidden_layers = hidden_layers,
+        rng = rng
         )
 
 episodes = []
@@ -57,12 +62,11 @@ results = [] # where can use `hook` to record the results
     reset!(wrapped_env)
     while !is_terminated(wrapped_env)
         RLBase.update!(nfsp, wrapped_env)
-
     end
 
     if episode % eval_every == 0
         push!(episodes, episode)
-        push!(results, reward(wrapped_env))
+        push!(results, RLZoo.nash_conv(nfsp, wrapped_env))
 
     end
 
@@ -72,5 +76,4 @@ end
 ENV["GKSwstype"]="nul" 
 using Plots
 
-savefig(scatter(episodes, results, xaxis=:log), "scatter_result")
 savefig(plot(episodes, results, xaxis=:log), "result")
