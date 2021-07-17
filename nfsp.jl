c9090faa713ed78e36af39f39da67ee1302e5a3b
peter <csherry1231@gmail.com>
2021-07-18 01:49:13 +0800

    modify the nfsp and add abstract_nfsp

--- nfsp.jl
+++ nfsp.jl
@@ -4,29 +4,38 @@ Neural Fictitious Self-Play (NFSP) agent implemented in Julia.
 See the paper https://arxiv.org/abs/1603.01121 for more details.
 """
 
-using ReinforcementLearning
-using StableRNGs
-using Flux
-using Flux.Losses
-using Distributions: TruncatedNormal
-
-include("average_learner.jl")
-include("supplement.jl")
-
-function initW(out_size, in_size)
-    d = TruncatedNormal(0, 1.0 / sqrt(in_size), -2, 2)
-    rand(d, (out_size, in_size))
-end
-
+# export NFSPAgent, NFSPAgents
 mutable struct NFSPAgents <: AbstractPolicy
-    agents::Dict{Any, Any}
+    agents::Dict{Any, AbstractPolicy}
 end
 
 mutable struct NFSPAgent <: AbstractPolicy
     η
-    rng::StableRNG
+    rng
     rl_agent::Agent
     sl_agent::Agent
+end
+include("supplement.jl")
+include("average_learner.jl")
+
+function initW(out_size, in_size)
+    mean, stddev = 0.0, 1.0 / sqrt(in_size)
+    lower, upper = (-2 * stddev - mean) / stddev, (2 * stddev - mean) / stddev
+    d = TruncatedNormal(mean, stddev, lower, upper)
+    rand(d, (out_size, in_size))
+end
+
+# DQN network relative functions 
+function build_dueling_network(network::Chain)
+    lm = length(network)
+    if !(network[lm] isa Dense) || !(network[lm-1] isa Dense)
+        error("The Qnetwork provided is incompatible with dueling.")
+    end
+    base = Chain([deepcopy(network[i]) for i=1:lm-2]...)
+    last_layer_dims = size(network[lm].W, 2)
+    val = Chain(deepcopy(network[lm-1]), Dense(last_layer_dims, 1))
+    adv = Chain([deepcopy(network[i]) for i=lm-1:lm]...)
+    return DuelingNetwork(base, val, adv)
 end
 
 function NFSPAgents(env::AbstractEnv; kwargs...)
@@ -41,25 +50,24 @@ end
 function NFSPAgent(
     env::AbstractEnv,
     player;
-
     # parameters setting
     # public parameters
     η = 0.1f0,
-    _device = Flux.gpu,
+    _device = Flux.cpu,
     Optimizer = Flux.Descent,
     rng = StableRNG(123),
     batch_size::Int = 128,
     learn_freq::Int = 128,
     min_buffer_size_to_learn::Int = 1000,
-    hidden_layers = (128, 128, 128, 128),
+    hidden_layers = (128, 128),
 
     # Reinforcement Learning(RL) agent parameters
+    rl_loss_func = mse,
+    rl_learning_rate = 0.01,
+    replay_buffer_capacity::Int = 200_000,
     ϵ_start = 0.06,
     ϵ_end = 0.001,
     ϵ_decay = 20_000_000,
-    rl_learning_rate = 0.01,
-    rl_loss_func = mse,
-    replay_buffer_capacity::Int = 200_000,
     discount_factor::Float32 = 1.0f0,
     update_target_network_freq::Int = 19200,
 
@@ -68,19 +76,13 @@ function NFSPAgent(
     reservoir_buffer_capacity::Int = 2_000_000
     )
 
-    # ignore chance_player.
-    while current_player(env) == chance_player(env)
-        env |> action_space |> rand |> env
-    end
-
     # base Neural network for training DQNLearner
-    ns = length(state(env, player))
-    na = length(action_space(env, player))
+    ns, na = length(state(env, player)), length(action_space(env, player))
     base_model = Chain(
         Dense(ns, hidden_layers[1], relu; init = initW),
         [Dense(hidden_layers[i], hidden_layers[i+1], relu; init = initW) 
             for i in 1:length(hidden_layers)-1]...,
-        Dense(hidden_layers[end], na; init = initW)
+        Dense(hidden_layers[end], na, relu; init = initW)
     )
 
     # RL agent
@@ -88,11 +90,13 @@ function NFSPAgent(
         policy = QBasedPolicy(
             learner = DQNLearner(
                 approximator = NeuralNetworkApproximator(
-                    model = build_dueling_network(base_model) |> _device,
+                    # model = build_dueling_network(base_model) |> _device,
+                    model = base_model |> _device,
                     optimizer = Optimizer(rl_learning_rate),
                 ),
                 target_approximator = NeuralNetworkApproximator(
-                    model = build_dueling_network(base_model) |> _device,
+                    # model = build_dueling_network(base_model) |> _device,
+                    model = base_model |> _device,
                 ),
                 γ = discount_factor,
                 loss_func = rl_loss_func,
@@ -121,12 +125,12 @@ function NFSPAgent(
         policy = QBasedPolicy(
             learner = AverageLearner(
                 approximator = NeuralNetworkApproximator(
-                    model = build_dueling_network(base_model) |> _device,
+                    model = base_model |> _device,
                     optimizer = Optimizer(sl_learning_rate),
                 ),
                 batch_size = batch_size,
                 update_freq = learn_freq,
-                min_replay_history = min_buffer_size_to_learn,
+                min_reservoir_history = min_buffer_size_to_learn,
                 rng = rng,
             ),
             explorer = WeightedSoftmaxExplorer(),
@@ -144,19 +148,6 @@ function NFSPAgent(
         sl_agent,
     )
     
-end
-
-# DQN network relative functions 
-function build_dueling_network(network::Chain)
-    lm = length(network)
-    if !(network[lm] isa Dense) || !(network[lm-1] isa Dense)
-        error("The Qnetwork provided is incompatible with dueling.")
-    end
-    base = Chain([deepcopy(network[i]) for i=1:lm-2]...)
-    last_layer_dims = size(network[lm].W, 2)
-    val = Chain(deepcopy(network[lm-1]), Dense(last_layer_dims, 1))
-    adv = Chain([deepcopy(network[i]) for i=lm-1:lm]...)
-    return DuelingNetwork(base, val, adv)
 end
 
 function RLBase.update!(π::NFSPAgent, env::AbstractEnv)
@@ -178,36 +169,25 @@ function RLBase.update!(π::NFSPAgent, env::AbstractEnv)
     end
 end
 
-function RLBase.update!(agents::NFSPAgents, env::AbstractEnv)
+function RLBase.update!(π::NFSPAgents, env::AbstractEnv)
     player = current_player(env)
     if player == chance_player(env)
         env |> legal_action_space |> rand |> env
     else
-        RLBase.update!(agents.agents[player], env)
+        RLBase.update!(π.agents[player], env)
     end
 end
 
-function (nfsp::NFSPAgent)(env::AbstractEnv)
+function (π::NFSPAgents)(env::AbstractEnv)
     player = current_player(env)
     if player == chance_player(env)
         env |> legal_action_space |> rand |> env
     else
-        nfsp.sl_agent(env)
-        # rand(nfsp.rng) < nfsp.η ? nfsp.rl_agent(env) : nfsp.sl_agent(env)
+        π.agents[player].sl_agent(env) |> env
     end
 end
 
-function RLBase.prob(nfsp::NFSPAgents, env::AbstractEnv, args...)
-    player = current_player(env)
-    agent = nfsp.agents[player]
-    a = agent.sl_agent
-    return prob(a.policy, env, args...)
-end
-
-function RLBase.prob(p::QBasedPolicy, env::AbstractEnv) 
-    # if typeof(p.explorer) == WeightedSoftmaxExplorer{Random._GLOBAL_RNG}
-    prob(p, env, ActionStyle(env))
-    # else
-    #     prob(p, env, ActionStyle(env)).p
-    # end
+function RLBase.prob(π::NFSPAgents, env::AbstractEnv, args...)
+    agent = π.agents[current_player(env)].sl_agent
+    prob(agent.policy, env, args...)
 end
--- supplement.jl
+++ supplement.jl
@@ -1,8 +1,50 @@
-export WeightedSoftmaxExplorer
+# abstract type NFSPAgents <: MultiAgentManager end
+
+# include("average_learner.jl")
+# include("nfsp.jl")
 
 using Random
 using StatsBase: sample, Weights
 using Flux: softmax
+
+function Base.run(
+    p::NFSPAgents,
+    env::AbstractEnv,
+    stop_condition = StopAfterEpisode(1),
+    hook = EmptyHook(),
+)
+    @assert NumAgentStyle(env) isa MultiAgent
+    @assert DynamicStyle(env) === SEQUENTIAL
+    @assert RewardStyle(env) === TERMINAL_REWARD
+    @assert ChanceStyle(env) === EXPLICIT_STOCHASTIC
+    @assert DefaultStateStyle(env) isa InformationSet
+
+    is_stop = false
+
+    while !is_stop
+        RLBase.reset!(env)
+        hook(PRE_EPISODE_STAGE, p, env)
+
+        while !is_terminated(env) # one episode
+            RLBase.update!(p, env)
+            hook(POST_ACT_STAGE, p, env)
+
+            if stop_condition(p, env)
+                is_stop = true
+                break
+            end
+        end # end of an episode
+
+        if is_terminated(env)
+            hook(POST_EPISODE_STAGE, p, env)
+        end
+    end
+    hook(POST_EXPERIMENT_STAGE, p, env)
+    hook
+end
+
+export WeightedSoftmaxExplorer
+
 
 """
     WeightedSoftmaxExplorer(;rng=Random.GLOBAL_RNG)
@@ -25,4 +67,4 @@ function (s::WeightedSoftmaxExplorer)(values::AbstractVector{T}, mask) where {T}
     s(values)
 end
 
-RLBase.prob(s::WeightedSoftmaxExplorer, values) = softmax(values)
+RLBase.prob(explorer::WeightedSoftmaxExplorer, values) = softmax(values)
